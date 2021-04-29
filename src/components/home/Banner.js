5705b9648f592e3d9a131954a9f5b6d4588f2c2d
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-04-29 01:54:08 -0300

    overlay

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Banner(props){
     return(
         <section class={style.banner}>
-            <div class={style.overlay}></div>
+            <div class={style.overlay}><p>a</p></div>
             <div class={style.container + ' container'}>
                 <div>
                     <p>Os senhores Aluado, Rabicho , Almofadinhas e Pontas tem o prazer em convidá-los para a Escola de Magia e Bruxaria de Hogwarts, aonde irão aprender inúmeras pegadinhas, feitiços, azarações e muito mais.</p>
