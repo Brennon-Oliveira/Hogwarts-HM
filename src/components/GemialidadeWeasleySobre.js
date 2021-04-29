9d721c795043c94537802c90b8d06692abec8760
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-04-29 18:05:28 -0300

    Correçao de ortografia

--- src/components/GemialidadeWeasleySobre.js
+++ src/components/GemialidadeWeasleySobre.js
@@ -8,7 +8,7 @@ export default function GemialidadeWeasleySobre(props){
             <div class={style.container + ' container'}>
                 <h2 class="title">Gemialidade Weasley</h2>
                 <p> A Gemialidade Weasley vai funcionar 1 vez na semana onde vocês poderão gastar seus galeões na lojinha.<br />
-                Como ganho galeões? Simples, a cada atividade que você participar do rpg vocês ganhará galões.</p>
+                Como ganho galeões? Simples, a cada atividade que você participar do rpg vocês ganhará galeões.</p>
                 <p> Você encontrará, artigos mágicos para aprontar com seus colegas e com as outras comunais. Para ativar o efeito basta falar com a Ali, Mandy ou Helloa</p>
                 <ul>
                     <li>GAMES: 100</li>
